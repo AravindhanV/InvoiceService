e4c9382192f69990156b87942c90d9613fbd5ca4
Aravindhan V <aravindhan.v@perfios.com>
2021-09-22 09:33:44 +0530

    ADDED: test to check if total fare calculated correctly over multiple rides

--- src/test/java/InvoiceService/InvoiceServiceTest.java
+++ src/test/java/InvoiceService/InvoiceServiceTest.java
@@ -7,21 +7,29 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class InvoiceServiceTest {
-    @Test
-    public void givenDistanceAndTime_ShouldReturnTotalFare() {
-    	InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
-    	double distance = 2.0;
-    	int time = 5;
-    	double fare = invoiceGenerator.calculateFare(distance,time);
-    	assertEquals(25,fare,0.0);
-    }
-    
-    @Test
-    public void givenLessDistanceAndTime_ShouldReturnMinimumFare() {
-    	InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
-    	double distance = 0.1;
-    	int time = 1;
-    	double fare = invoiceGenerator.calculateFare(distance, time);
-    	assertEquals(5,fare,0.0);
-    }
+	@Test
+	public void givenDistanceAndTime_ShouldReturnTotalFare() {
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		double distance = 2.0;
+		int time = 5;
+		double fare = invoiceGenerator.calculateFare(distance, time);
+		assertEquals(25, fare, 0.0);
+	}
+
+	@Test
+	public void givenLessDistanceAndTime_ShouldReturnMinimumFare() {
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		double distance = 0.1;
+		int time = 1;
+		double fare = invoiceGenerator.calculateFare(distance, time);
+		assertEquals(5, fare, 0.0);
+	}
+
+	@Test
+	public void givenMultipleWrites_ShouldReturnTotalFare() {
+		InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+		Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
+		double fare = invoiceGenerator.calculateFare(rides);
+		assertEquals(30, fare, 0.0);
+	}
 }
