3b4b6e3079182058881bc0086407247354111713
Aravindhan V <aravindhan.v@perfios.com>
2021-09-22 10:01:13 +0530

    ADDED: override to equals method

--- src/main/java/InvoiceService/InvoiceSummary.java
+++ src/main/java/InvoiceService/InvoiceSummary.java
@@ -2,6 +2,7 @@ package InvoiceService;
 
 public class InvoiceSummary {
 	private int noOfRides;
+
 	public double getTotalFare() {
 		return totalFare;
 	}
@@ -13,15 +14,29 @@ public class InvoiceSummary {
 	public int getNoOfRides() {
 		return noOfRides;
 	}
-	
+
 	public void setNoOfRides() {
 		this.noOfRides = noOfRides;
 	}
 
 	private double totalFare;
-	
-	public InvoiceSummary(int noOfRides,double totalFare) {
+	private double averageFare;
+
+	public InvoiceSummary(int noOfRides, double totalFare) {
 		this.noOfRides = noOfRides;
 		this.totalFare = totalFare;
+		this.averageFare = totalFare/noOfRides;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		// TODO Auto-generated method stub
+		if (this == o)
+			return true;
+		if (o == null || getClass() != o.getClass())
+			return false;
+		InvoiceSummary that = (InvoiceSummary) o;
+		return noOfRides == that.noOfRides && Double.compare(that.totalFare, this.totalFare) == 0
+				&& Double.compare(that.averageFare, this.averageFare) == 0;
 	}
 }
