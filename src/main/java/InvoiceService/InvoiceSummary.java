69c11c68819436985f008aa285a6ab63ec1cd517
Aravindhan V <aravindhan.v@perfios.com>
2021-09-22 09:51:41 +0530

    ADDED: constructor, getters and setters in InvoiceSummary

--- src/main/java/InvoiceService/InvoiceSummary.java
+++ src/main/java/InvoiceService/InvoiceSummary.java
@@ -1,5 +1,27 @@
 package InvoiceService;
 
 public class InvoiceSummary {
+	private int noOfRides;
+	public double getTotalFare() {
+		return totalFare;
+	}
 
+	public void setTotalFare(double totalFare) {
+		this.totalFare = totalFare;
+	}
+
+	public int getNoOfRides() {
+		return noOfRides;
+	}
+	
+	public void setNoOfRides() {
+		this.noOfRides = noOfRides;
+	}
+
+	private double totalFare;
+	
+	public InvoiceSummary(int noOfRides,double totalFare) {
+		this.noOfRides = noOfRides;
+		this.totalFare = totalFare;
+	}
 }
