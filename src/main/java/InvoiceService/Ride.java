d660d1cd96d752d873d68c77f4692761e4bca5f1
Aravindhan V <aravindhan.v@perfios.com>
2021-09-22 09:36:15 +0530

    ADDED: getters and setters for in Ride

--- src/main/java/InvoiceService/Ride.java
+++ src/main/java/InvoiceService/Ride.java
@@ -1,7 +1,27 @@
 package InvoiceService;
 
 public class Ride {
+	private double distance;
+	private double time;
+	
+	public double getTime() {
+		return time;
+	}
+
+	public void setTime(double time) {
+		this.time = time;
+	}
+
 	public Ride(double distance, int time) {
-		
+		this.distance = distance;
+		this.time = time;
+	}
+
+	public double getDistance() {
+		return distance;
+	}
+
+	public void setDistance(double distance) {
+		this.distance = distance;
 	}
 }
